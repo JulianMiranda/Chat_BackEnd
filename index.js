33fd02c09d58eb879971544a51d9b7c9012e1fea
Julian <julian@wayu2.com>
2020-11-28 18:23:57 -0500

    BackEnd Chat

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// DB Config
+require('./database/config').dbConnection();
+
+
 // App de Express
 const app = express();
+
+// Lectura y parseo del Body
+app.use( express.json() );
+
 
 // Node Server
 const server = require('http').createServer(app);
@@ -18,6 +26,9 @@ const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
 
 
+
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
 
 
 
--- sockets/socket.js
+++ sockets/socket.js
@@ -9,12 +9,12 @@ io.on('connection', client => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', ( payload ) => {
-        console.log('Mensaje', payload);
 
-        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
-
-    });
+    
+    // client.on('mensaje', ( payload ) => {
+    //     console.log('Mensaje', payload);
+    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    // });
 
 
 });
